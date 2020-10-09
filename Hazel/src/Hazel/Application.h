a68beaaf533231557fd83362a24cca3b13ce8285
Ruichao Zhang <zhangruichaotyler@gmail.com>
2020-10-08 22:37:21 -0400

    Setup basic Application and Entry Point

---
+++ Hazel/src/Hazel/Application.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Core.h"
+
+namespace Hazel
+{
+    class HAZEL_API Application
+    {
+    public:
+        Application();
+        virtual ~Application();
+
+        void Run();
+    };
+
+
+    //to be defined by client
+    Application* CreateApplication();
+}
+
---
+++ Hazel/src/Hazel/EntryPoint.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Application.h"
+#ifdef HZ_PLATFORM_WINDOWS
+
+extern Hazel::Application* Hazel::CreateApplication();
+
+int main(int argc, char** argv)
+{
+    
+    Hazel::Application* app = Hazel::CreateApplication();
+    app->Run();
+    delete app;
+}
+
+#endif
